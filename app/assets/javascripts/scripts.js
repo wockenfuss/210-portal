cdbbf1eb20a4a49f2f5852698f6992a7fcc8609f
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-05 13:20:35 -0700

    wrapped js in self invoking function)

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,7 +1,13 @@
-$(document).ready(function() {
-	(function() {
+(function(myApp, $, undefined) {
+	$(document).ready(function() {
+		bind();
+	});
+
+	var bind = function() {
 		$('.alert, .error, .notice').fadeIn('normal', function() {
 			$(this).delay(2500).fadeOut('slow');
 		});
-	})();
-});
+	};
+
+})(window.myApp = window.myApp || {}, jQuery);
+
