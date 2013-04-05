1bb0e4ea20d285c20a630fdf20bc2248bf61c6e3
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-05 13:51:32 -0700

    added first integration test for users

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,5 +3,14 @@ require 'spec_helper'
 describe "Users", :js => true do
   include Warden::Test::Helpers
 
+  let(:user) { FactoryGirl.create(:user) }
 
+  context "the root path" do
+	  context "when a user is not signed in" do
+	  	it "renders the sign in page" do
+	  		visit root_path
+	  		page.should have_content("Sign In")
+	  	end
+	  end
+	end
 end
