1617366eb73e07edefbeab47482d0f1bdb88aa46
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-08 16:14:10 -0700

    added admin manage option to view

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -7,8 +7,15 @@
 		$('#flash_alert, #flash_error, #flash_notice').fadeIn('normal', function() {
 			$(this).delay(2500).fadeOut('slow');
 		});
-		$('#dropdownTrigger').on('click', function(e){
-			$('#dropdown').slideToggle('normal');
+		$('#dropdownTrigger, #managerDropdownTrigger').on('click', function(e){
+			var $trigger = $(this);
+			var $dropdown = $trigger.next();
+			if ( !$dropdown.is(':visible') ) {
+				$('.dropdown:visible').slideToggle('fast');
+				var position = $trigger.position().left + $trigger.width() - $dropdown.width() + parseInt($trigger.css('margin-right'), 10) - 10;
+				$dropdown.css('left', position);
+			}
+			$dropdown.slideToggle('fast');
 		});
 	};
 
