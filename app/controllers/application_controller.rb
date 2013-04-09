20e06d6d3d7d19964b94b76be2f1d22bc75a52fd
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-08 19:04:44 -0700

    added catch for cancan exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to user_root_path(current_user), :alert => exception.message
+  end
+
   private
 	def after_sign_in_path_for(user)
 		user_root_path(:id => user.id)
