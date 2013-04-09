06942331614526fcf74206f7b6de662327cf6ead
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-08 19:06:03 -0700

    added index to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 
 	before_filter :authenticate_user!
-
+	load_and_authorize_resource
+	
 	def show
 		@user = User.find(params[:id])
 	end
+
+	def index
+		@user = current_user
+		@users = User.all
+	end
+
 end
