e3376cb98b871b22dcadcb3fda4659705a8bd4f0
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-10 18:28:49 -0700

    added js redirect and alert functions to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < ActionController::Base
     redirect_to user_root_path(current_user), :alert => exception.message
   end
 
+	def js_redirect_to(path)
+  	render js: %(window.location.href='#{path}') and return
+	end
+  
+  def js_alert(current_object)
+  	render "shared/errors", :locals => { :current_object => current_object }
+  end
+
   private
 	def after_sign_in_path_for(user)
 		user_root_path(:id => user.id)
