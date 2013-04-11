1cb9e1ae91b0cc251bcef183b1cded3821298c45
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-10 18:25:12 -0700

    added quiz factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,11 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     password "awesome1"
 	end
+
+	factory :quiz do
+		name { Faker::Lorem.word }
+		duration { rand(15) + 1 }
+		release_date { Time.now }
+		close_date { Time.now + 1000 }
+	end
 end
