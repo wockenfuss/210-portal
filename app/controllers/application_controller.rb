49fac03a49fe567454043f80c4739cf68abaa5f6
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-12 16:20:42 -0700

    added store location and redirect back functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def store_location
+    session[:return_to] = request.url
+  end
+
+  def redirect_back_or_default(default) 
+    redirect_to(session[:return_to] || default) 
+    session[:return_to] = nil 
+  end 
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to user_root_path(current_user), :alert => exception.message
   end
