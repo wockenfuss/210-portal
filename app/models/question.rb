724af5ae900c958dd097260c36a8311c928066a3
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-15 18:34:19 -0700

    modified attributes of question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,8 @@
 class Question < ActiveRecord::Base
-	has_and_belongs_to_many :quizzes#, :join_table => :questions_quizzes
+	has_and_belongs_to_many :quizzes, :join_table => :questions_quizzes
 	has_many :answers
 
-  attr_accessible :content, :correct_answer_id, :name
+  attr_accessible :content, :correct_answer_id, :name, :multiple_choice
+  attr_accessor :multiple_choice
   validates :content, :presence => true
 end
