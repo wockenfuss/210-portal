ee5ffb7272f56a8f124dcbf5b7eb320b8e06c4ba
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-15 18:54:24 -0700

    added ability to create, destroy, edit questions; added tests for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,26 +3,47 @@ class QuestionsController < ApplicationController
 
 	def new
 		@question = Question.new
+		@quiz = Quiz.find(params[:quiz_id])
+		respond_with @question, @quiz
 	end
 
 	def create
+		@quiz = Quiz.find(params[:quiz_id])
 		@question = Question.new(params[:question])
 		if @question.save
-
+			@quiz.questions << @question
+			@questions = @quiz.questions
+			respond_with @questions
 		else
-
+			js_alert(@question)
 		end
 	end
 
 	def edit
+		@question = Question.find(params[:id])
+		@quiz = @question.quizzes.first
+		respond_with @question, @quiz
 	end
 	
 	def update
+		@question = Question.find(params[:id])
+		if @question.update_attributes(params[:question])
+			flash[:notice] = "Question successfully updated"
+			js_redirect_to(edit_quiz_path(@question.quizzes.first))
+		else
+			js_alert(@question)
+		end
 	end
 
 	def index
 	end
 
-	def show
+	def destroy
+		@question = Question.find(params[:id])
+		@quiz = @question.quizzes.first
+		@question.destroy
+		flash[:notice] = "Successfully deleted"
+		js_redirect_to edit_quiz_path(@quiz)
 	end
+
 end
---
+++ spec/features/question_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "Question", :js => true do
+  include Warden::Test::Helpers
+
+  context "" do
+  	it "allows user to create questions" do
+  		pending
+  	end
+
+  	it "allows user to delete questions" do
+  		pending
+  	end
+
+  	it "allows user to update a question" do
+  		pending
+  	end
+
+  	it "displays questions in order by creation date by default" do
+  		pending
+  	end
+
+  	it "allows user to reorder questions" do
+  		pending
+  	end
+
+  	it "allows user to choose between multiple choice and essay questions" do
+  		pending
+  	end
+
+  	it "allows user to add possible answers to question" do
+  		pending
+  	end
+
+  	it "allows user to specify correct answer for multiple choice question" do
+  		pending
+  	end
+  end
+end
