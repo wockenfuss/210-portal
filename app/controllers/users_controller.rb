4e91c2d473649d0fdc5fe466fbabeef7760a137c
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-15 18:51:26 -0700

    added quiz show page; added current quiz display to user root page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 	
 	def show
 		@user = User.find(params[:id])
+		@quizzes = Quiz.where("release_date <= :current_time AND close_date >= :current_time", { :current_time => Time.now })
 	end
 
 	def index
