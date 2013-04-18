1d33a54f2dd227b756b9855b52e7e9d5b7e8f3c8
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-18 16:31:56 -0700

    disabled confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,15 @@
 class User < ActiveRecord::Base
   rolify
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, #:rememberable, 
-         :trackable, :validatable, :confirmable
+         :recoverable,
+         :trackable, :validatable#, :confirmable
 
   has_many :attempts
   has_many :responses
   has_one :gradebook
   
   after_create :create_gradebook
-
-  attr_accessible :email, :password, :password_confirmation, :gradebook_id
+  attr_accessible :email, :password, :password_confirmation
 
   def attempted?(quiz)
     self.attempts.where(:quiz_id => quiz.id) != []
