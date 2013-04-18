a94dc118ffa292b00ecd37ebee71e3a6c823521a
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-18 16:33:57 -0700

    tests for response model

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Response do
-  pending "add some examples to (or delete) #{__FILE__}"
+	subject { FactoryGirl.create(:response) }
+
+	it { should validate_presence_of :question_id }
+	it { should validate_presence_of :user_id }
+	it { should validate_presence_of :attempt_id }
+	it { should validate_presence_of :content }
+
+	[:answer_id, :content, :question_id, :user_id, :attempt_id, :points].each do |attr|
+		it { should respond_to attr }
+	end
 end
