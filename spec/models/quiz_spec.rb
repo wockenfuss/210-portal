4bf85505bcaf82b1ba4962bd70f94ab9b323e700
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-18 16:33:41 -0700

    tests for quiz model

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -15,4 +15,15 @@ describe Quiz do
 			FactoryGirl.build(:quiz, :release_date => Time.now + 10000).should_not be_valid
 		end
 	end	
+
+	describe "#possible_points" do
+		it "returns the total possible number of points for the quiz" do
+			quiz = FactoryGirl.create(:quiz)
+			question = FactoryGirl.create(:question)
+			question2 = FactoryGirl.create(:question)
+			quiz.questions << question
+			quiz.questions << question2
+			quiz.possible_points.should eq 10
+		end
+	end
 end
