23fdbb6528b1ba25663365947c6e2283db9e0256
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-18 16:34:22 -0700

    updated tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,35 @@ describe User do
 		it { should respond_to attr }
 	end
 
+	before(:each) do
+		@user = FactoryGirl.create(:user)
+		# @user.skip_confirmation!
+		@quiz = FactoryGirl.create(:quiz)
+	end
+
+	describe "#attempted?" do
+		it "returns true if user has attempted the given quiz" do	
+			attempt = FactoryGirl.create(:attempt, :user_id => @user.id, :quiz_id => @quiz.id)
+			@user.attempted?(@quiz).should be_true
+		end
+
+		it "returns false if user has not attempted quiz" do
+			@user.attempted?(@quiz).should be_false
+		end
+	end
+
+	describe "#graded_attempts" do
+
+		it "returns a list including only graded attempts" do
+			ungraded_params = {:user_id => @user.id, :quiz_id => @quiz.id}
+			graded_params = ungraded_params.merge(:graded => true)
+			attempt = FactoryGirl.create(:attempt, graded_params)
+			attempt2 = FactoryGirl.create(:attempt, graded_params)
+			attempt3 = FactoryGirl.create(:attempt, ungraded_params)
+			@user.graded_attempts.include?(attempt).should be_true
+			@user.graded_attempts.include?(attempt2).should be_true
+			@user.graded_attempts.include?(attempt3).should be_false
+		end
+	end
+
 end
