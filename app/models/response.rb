7d8e579fcfd68239d4045726100a9e1036eb1f19
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-22 20:47:35 -0700

    more multiple choice question functionality

--- app/models/response.rb
+++ app/models/response.rb
@@ -6,7 +6,11 @@ class Response < ActiveRecord::Base
 	validates :question_id, :presence => true
 	validates :user_id, :presence => true
 	validates :attempt_id, :presence => true
-	validates :content, :presence => true
+	# validates :content, :presence => true
 
   attr_accessible :answer_id, :content, :question_id, :user_id, :attempt_id, :points
+
+  def correct_answer?
+  	self.answer_id && self.answer_id == self.question.correct_answer_id
+  end
 end
