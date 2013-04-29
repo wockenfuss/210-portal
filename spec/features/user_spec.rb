669ee2ac1c77eab2b94d54f32a3398e9c999c086
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-04-29 14:29:03 -0700

    sketched out pending integration tests for user

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "User" do
+	require Warden::Test::Helpers
+
+	describe "users#show" do
+		context "when not signed in" do
+			it "requires user to sign in" do
+				pending
+			end
+		end
+
+		context "when signed in" do
+			context "when user is not authorized" do
+				it "displays a warning message" do
+					pending
+				end
+			end
+
+			context "when user is authorized" do
+				it "displays a list of currently open quizzes" do
+					pending
+				end
+
+				it "displays links for quizzes user has not yet attempted" do
+					pending
+				end
+
+				it "displays a message for quizzes that have been attempted" do
+					pending
+				end
+			end
+		end
+	end
+end
